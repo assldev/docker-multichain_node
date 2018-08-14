46a8391385877853972539334c4dc34aef81dc88
Akshit SINGLA <akshit.singla@amadeus.com>
2018-08-14 11:48:15 -0400

    Connecting the two nodes

--- scripts/create_chain.sh
+++ scripts/create_chain.sh
@@ -5,6 +5,9 @@ multichain-util create $chain_name \
 	-default-network-port=$default_network_port \
 	-default-rpc-port=$default_rpc_port 
 
+# UPDATE CONFIG
+sed -i "s/^anyone-can-connect.*/anyone-can-connect = true/" /root/.multichain/$chain_name/params.dat
+
 # PRINT CHAIN PARAMETERS
 cat ~/.multichain/$chain_name/params.dat
 
--- scripts/join_chain.sh
+++ scripts/join_chain.sh
@@ -2,3 +2,6 @@
 
 # CONNECT TO BLOCKCHAIN
 multichaind $chain_name@$admin_ip:$admin_port
+
+# START MULTICHAIN DAEMON
+multichaind $chain_name
